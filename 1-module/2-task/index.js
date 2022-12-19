7284dae57c27560beaf6f28849c58e64652b139d
Tim <tnovgorodoff@MacBook-Air.local>
2022-12-19 03:32:01 +0300

    решение 1-module/2-task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,12 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+    if (name != null && name != '' && name.includes(" ") === false && name.length >= 4) {
+      //строка не null && строка не пустая && строка без пробелов && длина >= 4 символа
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function sayHello() {
