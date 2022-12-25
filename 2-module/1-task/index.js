a5b22d635db496f94df4a92c3c425d13da8ddb07
Tim <tnovgorodoff@MacBook-Air.local>
2022-12-26 02:55:00 +0300

    решение 2-module/1-task, 2-task и 3-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let salary of Object.values(salaries)) {
+    if (typeof (salary) === 'number' && isFinite(salary)) {
+      sum += salary;
+    }
+  }
+  return sum;
 }
